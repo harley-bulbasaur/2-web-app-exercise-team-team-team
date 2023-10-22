0245f4f39b9c2bd59200493837e09529fe406b5a
Steven Liu <khl446@nyu.edu>
2023-10-22 19:13:16 -0400

    acc_register.html pushing

--- db.py
+++ db.py
@@ -6,6 +6,26 @@ client = MongoClient('mongodb://admin:secret@127.0.0.1:27017/')
 db = client['task_board']
 tasks_collection = db['tasks']
 
+db = client['Users_DataBase']
+user_collection = db['users']
+
+def get_users_collection():
+    return user_collection
 
 def get_tasks_collection():
     return tasks_collection
+
+def insert_user(username, password, email):
+    try:
+        user_document = {
+            'username': username,
+            'password': password,
+            'email': email
+        }
+
+        user_collection.insert_one(user_document)
+        return user_document['_id']
+
+    except Exception as e:
+        print(f"An error occurred during user registration: {e}")
+        return None  

84cf7ac76ac19952e5837ade92cba71d080ebb62
wwxihan2 <zl3499@nyu.edu>
2023-10-22 16:14:49 -0400

    update user stories

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for
+import pymongo
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
+from db import get_tasks_collection
+
 app = Flask(__name__, static_folder='public')
+
 
 # Connect to db.py
 tasks_collection = get_tasks_collection()
+
 
 @app.route('/')
 def home():
     tasks = tasks_collection.find()
     return render_template('home.html', tasks=tasks)
+
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_task():
@@ -19,21 +25,23 @@ def add_task():
         description = request.form['description']
         priority = request.form['priority']
         due_date = request.form['due_date']
-        
+
         tasks_collection.insert_one({
             'title': title,
             'description': description,
             'priority': priority,
             'due_date': due_date
         })
-        
+
         return redirect(url_for('home'))
-    
+
     return render_template('add_task.html')
+
 
 @app.route('/public/<path:filename>')
 def custom_static(filename):
     return send_from_directory(app.static_folder, filename)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
